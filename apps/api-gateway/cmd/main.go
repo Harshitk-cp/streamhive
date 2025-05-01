069674d9776b603f64e2e58de184959555d20897
Harshit <harshitk.cp@gmail.com>
2025-05-01 17:52:53 +0530

    feat: initialising main

--- apps/api-gateway/cmd/main.go
+++ apps/api-gateway/cmd/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	pb "github.com/Harshitk-cp/streamhive/libs/proto/signaling"
+
 	"google.golang.org/grpc"
 )
+
+type server struct {
+	pb.SignalingServiceServer
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
@@ -13,9 +20,17 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-
+	pb.RegisterSignalingServiceServer(s, &server{})
 	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func (s *server) SendSignal(ctx context.Context, req *pb.SignalRequest) (*pb.SignalResponse, error) {
+	log.Printf("Received signal: SessionID=%s Payload=%s", req.GetSessionId(), req.GetPayload())
+
+	return &pb.SignalResponse{
+		Status: "Received",
+	}, nil
 }
